259996a56f34a7ad3dbd81c5861f965de0a078f4
almondtools <mandelst@gmail.com>
2017-03-15 07:27:24 +0100

    markdown import supports redirecting links

--- src/main/java/com/almondtools/ctpweb/IncludeMarkdown.java
+++ src/main/java/com/almondtools/ctpweb/IncludeMarkdown.java
@@ -11,7 +11,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.pegdown.LinkRenderer;
 import org.pegdown.PegDownProcessor;
+import org.pegdown.ast.ExpLinkNode;
+import org.pegdown.ast.Node;
 
 import com.almondtools.comtemplate.engine.ArgumentRequiredException;
 import com.almondtools.comtemplate.engine.Scope;
@@ -25,53 +28,66 @@ import com.almondtools.comtemplate.processor.TemplateProcessor;
 
 public class IncludeMarkdown extends TemplateDefinition {
 
-	public static final String NAME = "includeMarkdown";
-	public static final String SOURCE = "source";
-	public static final String CHARSET = "charset";
+    public static final String NAME = "includeMarkdown";
+    public static final String SOURCE = "source";
+    public static final String CHARSET = "charset";
+    public static final String LINKBASE = "linkbase";
 
-	private static final String DEFAULT_CHARSET = "utf-8";
-	
-	private PegDownProcessor processor;
-	
-	public IncludeMarkdown() {
-		super(NAME, SOURCE, param(CHARSET, string(DEFAULT_CHARSET)));
-		this.processor = new PegDownProcessor();
-	}
+    private static final String DEFAULT_CHARSET = "utf-8";
 
-	@Override
-	public TemplateImmediateExpression evaluate(TemplateInterpreter interpreter, Scope parent, List<TemplateVariable> arguments) {
-		List<TemplateVariable> variables = createVariables(arguments);
-		
-		String source = findVariable(SOURCE, variables)
-			.orElseThrow(() -> new ArgumentRequiredException(SOURCE))
-			.getValue().apply(interpreter, parent)
-			.getText();
-		String charset = findVariable(CHARSET, variables)
-			.orElse(var(CHARSET, string(DEFAULT_CHARSET)))
-			.getValue().apply(interpreter, parent)
-			.getText();
-		
-		String base = parent.resolveContextVariable(TemplateProcessor.SOURCE)
-			.getValue().apply(interpreter, parent)
-			.getText();
-		
-		try {
-			String document = loadDocument(base, source, charset);
-			String html = processor.markdownToHtml(document);
-			return new RawText(html);
-		} catch (IOException e) {
-			return new IOResolutionError(e.getMessage());
-		}
-		
-	}
+    private PegDownProcessor processor;
 
-	public String loadDocument(String base, String source, String charset) throws IOException {
-		Path file = Paths.get(source);
-		if (!Files.exists(file)) {
-			file = Paths.get(base, source);
-		}
-		
-		return new String(Files.readAllBytes(file), Charset.forName(charset));
-	}
+    public IncludeMarkdown() {
+        super(NAME, SOURCE, param(CHARSET, string(DEFAULT_CHARSET)));
+        this.processor = new PegDownProcessor();
+    }
+
+    @Override
+    public TemplateImmediateExpression evaluate(TemplateInterpreter interpreter, Scope parent, List<TemplateVariable> arguments) {
+        List<TemplateVariable> variables = createVariables(arguments);
+
+        String source = findVariable(SOURCE, variables)
+            .orElseThrow(() -> new ArgumentRequiredException(SOURCE))
+            .getValue().apply(interpreter, parent)
+            .getText();
+        String charset = findVariable(CHARSET, variables)
+            .orElse(var(CHARSET, string(DEFAULT_CHARSET)))
+            .getValue().apply(interpreter, parent)
+            .getText();
+
+        String base = parent.resolveContextVariable(TemplateProcessor.SOURCE)
+            .getValue().apply(interpreter, parent)
+            .getText();
+
+        String linkbase = parent.resolveContextVariable(LINKBASE) == null
+            ? ""
+            : parent.resolveContextVariable(LINKBASE)
+                .getValue().apply(interpreter, parent)
+                .getText();
+
+        try {
+            String document = loadDocument(base, source, charset);
+            String html = processor.markdownToHtml(document, new LinkRenderer() {
+                public Rendering render(org.pegdown.ast.ExpLinkNode node, String text) {
+                    Node child = node.getChildren().isEmpty() ? null : node.getChildren().get(0);
+                    node = new ExpLinkNode(node.title, linkbase + node.url, child);
+                    return super.render(node, text);
+                };
+            });
+            return new RawText(html);
+        } catch (IOException e) {
+            return new IOResolutionError(e.getMessage());
+        }
+
+    }
+
+    public String loadDocument(String base, String source, String charset) throws IOException {
+        Path file = Paths.get(source);
+        if (!Files.exists(file)) {
+            file = Paths.get(base, source);
+        }
+
+        return new String(Files.readAllBytes(file), Charset.forName(charset));
+    }
 
 }
